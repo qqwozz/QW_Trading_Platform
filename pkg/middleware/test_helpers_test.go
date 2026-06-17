035dc2d0199c59c325b1ff5d41c172d5dd8f86cb
qqwozz <offconix@gmail.com>
2026-06-17 15:07:41 +0300

    Fix CI: replace golangci-lint with go vet + staticcheck, remove unused type

--- pkg/middleware/test_helpers_test.go
+++ pkg/middleware/test_helpers_test.go
@@ -2,8 +2,6 @@ package middleware
 
 import "context"
 
-type testContextKey string
-
 func newContextWithValue(key contextKey, value interface{}) context.Context {
 	return context.WithValue(context.Background(), key, value)
 }
